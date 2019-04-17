72a5543418b74816a81fc5d123d855fbbbffaf3c
harry <zh_harry@163.com>
2019-04-17 09:00:38 +0800

    feature:modify batch event bus

--- src/main/java/com/sparrow/core/algorithm/bus/OverflowProcessor.java
+++ src/main/java/com/sparrow/core/algorithm/bus/OverflowProcessor.java
@@ -14,14 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.sparrow.core.algorithm.bucket;
+package com.sparrow.core.algorithm.bus;
 
 import java.util.List;
 
 /**
  * @author by harry
  */
-public interface Overflow<T> {
+public interface OverflowProcessor<T> {
     /**
      *
      * @param list
